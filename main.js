dbabdb93d34ab66ae18899a27ff243a285e7ea0a
BlankCanvasTM <BlankCanvasTM@users.noreply.github.com>
2023-02-02 10:01:10 +0000

    Navbar part 2

--- main.js
+++ main.js
@@ -1,5 +1,30 @@
 const hamburger = document.querySelector('.hamburger');
+const hamburgerMenu = document.querySelector('.hamburgerMenu');
 
 hamburger.addEventListener('click', function (){
     hamburger.classList.toggle('is-active');
+    hamburgerMenu.classList.toggle('active');
 });
+
+const searchBtn = document.querySelector('.searchBtn'); 
+const searchBox = document.querySelector('.searchbox');
+const closeBtn = document.querySelector('.closeBtn');
+
+
+searchBtn.addEventListener('click', function () {
+    searchBox.classList.add('active');
+    hamburger.classList.add('hiddenHamburger');
+})
+
+closeBtn.addEventListener('click', function () {
+    searchBox.classList.remove('active');
+    hamburger.classList.remove('hiddenHamburger');
+})
+
+
+
+
+//let searchBtn = document.querySelector('.searchBtn'); 
+
+
+
